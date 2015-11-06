8de656fc0e0e154de2e11f6610db5542f0910853
root <root@tcb10153.tcloud.infra>
2015-11-06 16:17:25 +0900

    update1 server.js text

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-var express = require('express')
-    , http = require('http');
-
+var express = require('express'), http = require('http');
 var app = express();
-
 var port = process.env.PORT || 3616;
 var host = process.env.HOST || "0.0.0.0";
-
 var server = http.createServer(app).listen(port, host, function() {
     console.log("Server listening to %s:%d within %s environment",
         host, port, app.get('env'));
 });
-
 app.use(express.static(__dirname+'/'));
-
 console.log("Start use..............");
